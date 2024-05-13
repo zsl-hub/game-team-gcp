460646f65f572e059db8de60f8e0bf05813c1227
Kotieta <mikitaleuchyk@gmail.com>
2024-05-13 12:37:49 +0200

    Naprawienie błędów strony głównej

--- checkers-frontend/src/views/Menu.vue
+++ checkers-frontend/src/views/Menu.vue
@@ -8,93 +8,107 @@
   <Card id="List">
     <template #title>ROOM LIST</template>
     <template #content>
+      <p class="m-0">
+      <div id="Room">
+        <div id="Left">
+          <div id="RoomName" name="RoomName">RoomName 1</div>
+          <br>
+          <div id="Amount" name="Amount">Players: 1/2</div>
+        </div>
+
+        <div id="Right">
+          <RouterLink to="/Game"> <Button label="Submit" id="Join">JOIN</Button> </RouterLink>
+        </div>
+
+
+
+
+      </div><br><br>
+
+      <Divider id="Divider" /><br><br>
+
+      <div id="Room">
+        <div id="Left">
+          <div id="RoomName" name="RoomName">RoomName 2</div>
+          <br>
+          <div id="Amount" name="Amount">Players: 2/2</div>
+        </div>
+
+        <div id="Right">
+          <RouterLink to="/Game"> <Button label="Submit" id="Join">JOIN</Button> </RouterLink>
+        </div>
+
+
+      </div>
+      </p>
+    </template>
+  </Card>
+
+
+  <Card id="Form">
+    <template #title>BOARD</template>
+    <template #content>
+      <p class="m-0">
+
+        <InputText name="username" id="username" aria-describedby="username-help" /><br>
+        <small id="username-help">Enter board name.</small><br><br>
+
+        <SelectButton name="SelectButton" v-model="value" :options="options" aria-labelledby="basic" /><br>
+
+        <div class="CreateButtonContainer">
+          <Button name="CreateButton" class="CreateButton" label="Create" text raised />
+        </div>
+
+      </p>
+    </template>
+  </Card>
+
+
+  <Card id="Rules">
+    <template #title>Rules</template>
+    <template #content>
         <p class="m-0">
-          <div id="Room">
-            <div id="Left">
-              <div id="RoomName" name="RoomName">RoomName 1</div>
-              <br>
-              <div id="Amount" name="Amount">Players: 1/2</div>
-            </div>
-
-            <div id="Right">
-              <RouterLink to="/Game"> <Button label="Submit" id="Join">JOIN</Button> </RouterLink>
-            </div>
-
-            
-
-            
-          </div><br><br>
-
-          <Divider id="Divider"/><br><br>
-
-          <div id="Room">
-            <div id="Left">
-              <div id="RoomName" name="RoomName">RoomName 2</div>
-              <br>
-              <div id="Amount" name="Amount">Players: 2/2</div>
-            </div>
-
-            <div id="Right">
-              <RouterLink to="/Game"> <Button label="Submit" id="Join">JOIN</Button> </RouterLink>
-            </div>
-
-            
-          </div>
+          <ul>
+            <li>Board 8x8</li>
+            <li>Short moves only</li>
+            <li>Cannot capture backwards</li>
+            <li>Capture - choose any</li>
+            <li>Red is playing first</li>
+          </ul>
         </p>
     </template>
   </Card>
 
-  <div id="Form">
-    <div id="formHeader">Board</div>
- 
-                <InputText name="username" id="username" v-model="value" aria-describedby="username-help" /><br>
-            <small  id="username-help">Enter your username to reset your password.</small><br><br>
-        
-                <SelectButton   name="SelectButton" v-model="value" :options="options" aria-labelledby="basic" /><br>
-            <div class="CreateButtonContainer">
-                <Button name="CreateButton" class="CreateButton" label="Create" text raised /> 
-</div>
-        
-  </div>
 
+    
 
-  <div id="Rules">
-    Rules:
-
-    <ul>
-      <li>Board 8x8</li>
-      <li>Short moves only</li>
-      <li>Cannot capture backwards</li>
-      <li>Capture - choose any</li>
-      <li>Red is playing first</li>
-    </ul>
-  </div> 
-  
 </template>
 
 
 
 <style>
-  *{
+  * {
     color: rgba(255, 255, 255, 0.87);
   }
-  
-  header{
+
+  header {
     text-align: center;
     font-size: 200%;
     font-weight: bold;
     height: 4vh;
   }
 
-  #List{
+  #List {
     float: left;
     width: 30%;
     height: 96vh;
     padding: 10px;
     overflow: auto;
+    border: 1px solid #818cf8;
+    border-radius: 6px 0 0 6px;
   }
 
-  #Room{
+  #Room {
     border: 3px solid #818cf8;
     border-radius: 10px;
     padding: 1vw;
@@ -102,55 +116,60 @@
     grid-template-columns: 75% 25%;
   }
 
-  #Join{
+  #Join {
     margin: 5% 0 5% 0;
   }
 
-  #Form{
+  #Form {
     height: 60vh;
     float: right;
     width: 70%;
-    
+    border: 1px solid #818cf8;
     padding: 10px;
+    border-radius: 0 6px 0 0;
   }
 
-  #formHeader{
+  #formHeader {
     text-align: center;
     font-size: 150%;
     font-weight: bold;
   }
-  .CreateButtonContainer{
+
+  .CreateButtonContainer {
     text-align: center;
-    margin-top: 32vh;
-    
+    margin-top: 10%;
   }
-  .CreateButton{
-    border: 2px solid blue;
+
+  .CreateButton {
+    border: 3px solid #818cf8;
     border-radius: 10px;
     font-size: 200%;
-    width: 8vw;
+    width: 12vw;
     height: 8vh;
+    background: #818cf8;
   }
-  #Rules{
+
+  #Rules {
     float: right;
-    border: 3px solid black;
+    border: 1px solid #818cf8;
+    border-radius: 0 0 6px 0;
     width: 70%;
     height: 36vh;
     padding: 10px;
   }
-
 </style>
 
 <script setup>
+
   import Card from 'primevue/card';
   import Divider from 'primevue/divider';
   import Button from 'primevue/button';
+
   import SelectButton from 'primevue/selectbutton';
   import InputText from 'primevue/inputtext'
+
   import { ref } from 'vue';
-  const options = ref(['Black', 'Red', 'Random']);
-
-
-
+  const value = ref('Random');
+  const options = ref(['Random', 'Red', 'Black']);
 
 </script>
