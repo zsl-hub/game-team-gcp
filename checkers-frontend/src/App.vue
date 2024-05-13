afa9b418a5eb82d15ceeb07e6cb5b71f00fce33c
Kotieta <mikitaleuchyk@gmail.com>
2024-05-13 11:08:34 +0200

    Dodanie stylu do listy z planszami

--- checkers-frontend/src/App.vue
+++ checkers-frontend/src/App.vue
@@ -2,34 +2,9 @@
 import { RouterLink, RouterView } from 'vue-router'
 </script>
 <template>
-  <header>
-      <nav>
-          
-      </nav>
-  </header>
   <RouterView />
   
   
 </template>
 <style scoped>
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-nav a:first-of-type {
-  border: 0;
-}
 </style>
