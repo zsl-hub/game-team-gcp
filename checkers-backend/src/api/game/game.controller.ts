7e98997b4f42fa1058525d6824dd7ddcccdfc0c0
Maciej Grzesiak <107268659+Mistermakos@users.noreply.github.com>
2024-05-14 08:26:15 +0200

    Added delete option (#51)

--- checkers-backend/src/api/game/game.controller.ts
+++ checkers-backend/src/api/game/game.controller.ts
@@ -1,19 +1,18 @@
 import { Datastore } from '@google-cloud/datastore';
-import { BadRequestException, Body, Controller, Get, NotFoundException, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, NotFoundException, Param, Post } from '@nestjs/common';
 import { ApiService } from '../api.service';
 import { createGame } from './game.dto'
 
 @Controller('api/v1/Game')
 export class GameController {
-    constructor(private readonly apiService: ApiService) { }
+    constructor(private readonly api: ApiService) { }
     datastore = new Datastore({ databaseId: 'checkers-datastore', projectId: "checkers-zsl" });
 
     @Get()
     async findAll(): Promise<object> {
         try {
-            const query = this.datastore.createQuery('Game')
-            const result = await this.datastore.runQuery(query)
-            return await this.apiService.ApiSuccessData({ result })
+            const result = await this.api.findAll("Game")
+            return await this.api.ApiSuccessData({ result })
         }
         catch (err) { throw new NotFoundException("Couldn't find all games")}
     }
@@ -21,14 +20,23 @@ export class GameController {
     @Post()
     async add(@Body() body: createGame): Promise<string> {
         try {
-            const taskKey = await this.datastore.key('Game');
+            const taskKey =  this.datastore.key('Game');
             const entity = {
                 key: taskKey,
                 data: { body },
             };
             await this.datastore.save(entity);
-            return await this.apiService.ApiSuccessNoData();
+            return await this.api.ApiSuccessNoData();
         }
         catch (err) {throw new BadRequestException('Something bad happened')}
+    }
+    
+    @Delete(":id")
+    async delete(@Param('id') id: string): Promise<string> {
+        try {
+            await this.api.delete(id,"Game")
+            return await this.api.ApiSuccessNoData();
+        }
+        catch (err) {console.log(err); throw new BadRequestException('Something bad happened')}
     }
 }
