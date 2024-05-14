368d015b14ac8a8310b62f52974535b059dcb607
Maciej Grzesiak <107268659+Mistermakos@users.noreply.github.com>
2024-05-14 09:02:35 +0200

    Added filtering by id (gcp's key) (#52)

--- checkers-backend/src/api/game/game.controller.ts
+++ checkers-backend/src/api/game/game.controller.ts
@@ -1,10 +1,11 @@
 import { Datastore } from '@google-cloud/datastore';
-import { BadRequestException, Body, Controller, Delete, Get, NotFoundException, Param, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, NotFoundException, Param, Post, Put } from '@nestjs/common';
 import { ApiService } from '../api.service';
 import { createGame } from './game.dto'
 
 @Controller('api/v1/Game')
 export class GameController {
+    constructor(private readonly api: ApiService) { }
     constructor(private readonly api: ApiService) { }
     datastore = new Datastore({ databaseId: 'checkers-datastore', projectId: "checkers-zsl" });
 
@@ -13,13 +14,25 @@ export class GameController {
         try {
             const result = await this.api.findAll("Game")
             return await this.api.ApiSuccessData({ result })
+            const result = await this.api.findAll("Game")
+            return await this.api.ApiSuccessData({ result })
         }
         catch (err) { throw new NotFoundException("Couldn't find all games")}
+    }
+
+    @Get(":id")
+    async findOne(@Param('id') id: string): Promise<object> {
+        try {
+            const result = await this.api.findOne(id, "Game")
+            return await this.api.ApiSuccessData({ result })
+        }
+        catch (err) { throw new NotFoundException("Couldn't find all gamemoves")}
     }
 
     @Post()
     async add(@Body() body: createGame): Promise<string> {
         try {
+            const taskKey =  this.datastore.key('Game');
             const taskKey =  this.datastore.key('Game');
             const entity = {
                 key: taskKey,
@@ -27,8 +40,18 @@ export class GameController {
             };
             await this.datastore.save(entity);
             return await this.api.ApiSuccessNoData();
+            return await this.api.ApiSuccessNoData();
         }
         catch (err) {throw new BadRequestException('Something bad happened')}
+    }
+    
+    @Delete(":id")
+    async delete(@Param('id') id: string): Promise<string> {
+        try {
+            await this.api.delete(id,"Game")
+            return await this.api.ApiSuccessNoData();
+        }
+        catch (err) {console.log(err); throw new BadRequestException('Something bad happened')}
     }
     
     @Delete(":id")
--- checkers-backend/src/api/gamemoves/gamemoves.controller.ts
+++ checkers-backend/src/api/gamemoves/gamemoves.controller.ts
@@ -1,10 +1,11 @@
 import { Datastore } from '@google-cloud/datastore';
-import { BadRequestException, Body, Controller, Delete, Get, NotFoundException, Param, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, NotFoundException, Param, Post, Put } from '@nestjs/common';
 import { ApiService } from '../api.service';
 import { createGameMove } from './gamemoves.dto'
 
 @Controller('api/v1/GameMoves')
 export class GameMovesController {
+    constructor(private readonly api: ApiService) { }
     constructor(private readonly api: ApiService) { }
     datastore = new Datastore({ databaseId: 'checkers-datastore', projectId: "checkers-zsl" });
 
@@ -17,9 +18,19 @@ export class GameMovesController {
         catch (err) { throw new NotFoundException("Couldn't find all gamemoves")}
     }
 
+    @Get(":id")
+    async findOne(@Param('id') id: string): Promise<object> {
+        try {
+            const result = await this.api.findOne(id, "GameMove")
+            return await this.api.ApiSuccessData({ result })
+        }
+        catch (err) { throw new NotFoundException("Couldn't find all gamemoves")}
+    }
+
     @Post()
     async add(@Body() body: createGameMove): Promise<string> {
         try {
+            const taskKey = this.datastore.key('GameMove');
             const taskKey = this.datastore.key('GameMove');
             const entity = {
                 key: taskKey,
@@ -27,9 +38,20 @@ export class GameMovesController {
             };
             await this.datastore.save(entity);
             return await this.api.ApiSuccessNoData();
+            return await this.api.ApiSuccessNoData();
         }
         catch (err) {throw new BadRequestException('Something bad happened')}
     }
+    
+    @Delete(":id")
+    async delete(@Param('id') id: string): Promise<string> {
+        try {
+            await this.api.delete(id,"GameMove")
+            return await this.api.ApiSuccessNoData();
+        }
+        catch (err) {console.log(err); throw new BadRequestException('Something bad happened')}
+    }
+}
     
     @Delete(":id")
     async delete(@Param('id') id: string): Promise<string> {
