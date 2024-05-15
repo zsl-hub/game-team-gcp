0094b9057e9bdfe79c4f00bdcbbf97fbad7ac3d2
Hayk-Tovmasyan <116221447+Hayk-Tovmasyan@users.noreply.github.com>
2024-05-15 14:19:20 +0200

    Update Menu.vue

--- checkers-frontend/src/views/Menu.vue
+++ checkers-frontend/src/views/Menu.vue
@@ -7,23 +7,23 @@
     <template #title>ROOM LIST</template>
     <template #content>
       <p class="m-0">
-       
-          <div v-for="(room, index) in rooms" :key="index" id="Room">
-            <div class="Left">
-              <div class="RoomName">RoomName {{ index + 1 }}</div>
-              <div class="Amount">Players: {{ room.players }}/2</div>
-            </div>
-            <div class="Right">
-              <RouterLink to="/Game">
-                <Button label="Submit" id="Join">JOIN</Button>
-              </RouterLink>
-            </div>
-            
+
+      <div v-for="(room, index) in rooms" :key="index" id="Room">
+        <div class="Left">
+          <div class="RoomName">RoomName {{ index + 1 }}</div>
+          <div class="Amount">Players: {{ room.players }}/2</div>
         </div>
-        
-       
-        
-        
+        <div class="Right">
+          <RouterLink to="/Game">
+            <Button label="Submit" id="Join">JOIN</Button>
+          </RouterLink>
+        </div>
+
+      </div>
+
+
+
+
       </p>
     </template>
   </Card>
@@ -35,9 +35,9 @@
         <InputText name="username" id="username" aria-describedby="username-help" /><br>
         <small id="username-help">Enter board name.</small><br><br>
         <SelectButton name="SelectButton" v-model="value" :options="options" aria-labelledby="basic" /><br>
-        <div class="CreateButtonContainer">
-          <Button name="CreateButton" class="CreateButton" label="Create" text raised @click="createRoom()"></Button>
-        </div>
+      <div class="CreateButtonContainer">
+        <Button name="CreateButton" class="CreateButton" label="Create" text raised @click="createRoom()"></Button>
+      </div>
       </p>
     </template>
   </Card>
@@ -46,13 +46,13 @@
     <template #title>Rules</template>
     <template #content>
       <p class="m-0">
-        <ul>
-          <li>Board 8x8</li>
-          <li>Short moves only</li>
-          <li>Cannot capture backwards</li>
-          <li>Capture - choose any</li>
-          <li>Red is playing first</li>
-        </ul>
+      <ul>
+        <li>Board 8x8</li>
+        <li>Short moves only</li>
+        <li>Cannot capture backwards</li>
+        <li>Capture - choose any</li>
+        <li>Red is playing first</li>
+      </ul>
       </p>
     </template>
 
@@ -60,19 +60,19 @@
 </template>
 
 <script setup>
-  import { ref } from 'vue';
-  import Card from 'primevue/card';
+import { ref } from 'vue';
+import Card from 'primevue/card';
 
-  import Button from 'primevue/button';
-  import SelectButton from 'primevue/selectbutton';
-  import InputText from 'primevue/inputtext';
-  
-  let divider = document.querySelector('.divider')
-  const options = ref(['Random', 'Red', 'Black']);
+import Button from 'primevue/button';
+import SelectButton from 'primevue/selectbutton';
+import InputText from 'primevue/inputtext';
 
-  const rooms = ref([
-  
-  
+
+const options = ref(['Random', 'Red', 'Black']);
+
+const rooms = ref([
+
+
 ]);
 const newRoomName = ref('');
 
@@ -80,14 +80,14 @@ const newRoomName = ref('');
 
 const createRoom = () => {
   rooms.value.push({ name: newRoomName.value, players: 1 });
-  newRoomName.value = ''; 
-  
-  
+  newRoomName.value = '';
+
+
 };
 
 
 
-  
+
 </script>
 
 
@@ -95,78 +95,74 @@ const createRoom = () => {
 
 
 <style>
-   * {
-    color: rgba(255, 255, 255, 0.87);
-  }
+* {
+  color: rgba(255, 255, 255, 0.87);
+}
 
-  header {
-    text-align: center;
-    font-size: 200%;
-    font-weight: bold;
-    height: 4vh;
-  }
+header {
+  text-align: center;
+  font-size: 200%;
+  font-weight: bold;
+  height: 4vh;
+}
 
-  #List {
-    float: left;
-    width: 30%;
-    height: 96vh;
-    padding: 10px;
-    overflow: auto;
-    border: 1px solid #818cf8;
-    border-radius: 6px 0 0 6px;
-  }
+#List {
+  float: left;
+  width: 30%;
+  height: 96vh;
+  padding: 10px;
+  overflow: auto;
+  border: 1px solid #818cf8;
+  border-radius: 6px 0 0 6px;
+}
 
-  #Room {
-    border: 3px solid #818cf8;
-    border-radius: 10px;
-    padding: 1vw;
-    display: grid;
-    grid-template-columns: 75% 25%;
-  }
+#Room {
+  border: 3px solid #818cf8;
+  border-radius: 10px;
+  padding: 1vw;
+  display: grid;
+  grid-template-columns: 75% 25%;
+}
 
-  #Join {
-    margin: 5% 0 5% 0;
-  }
+#Join {
+  margin: 5% 0 5% 0;
+}
 
-  #Form {
-    height: 60vh;
-    float: right;
-    width: 70%;
-    border: 1px solid #818cf8;
-    padding: 10px;
-    border-radius: 0 6px 0 0;
-  }
+#Form {
+  height: 60vh;
+  float: right;
+  width: 70%;
+  border: 1px solid #818cf8;
+  padding: 10px;
+  border-radius: 0 6px 0 0;
+}
 
-  #formHeader {
-    text-align: center;
-    font-size: 150%;
-    font-weight: bold;
-  }
+#formHeader {
+  text-align: center;
+  font-size: 150%;
+  font-weight: bold;
+}
 
-  .CreateButtonContainer {
-    text-align: center;
-    margin-top: 10%;
-  }
+.CreateButtonContainer {
+  text-align: center;
+  margin-top: 10%;
+}
 
-  .CreateButton {
-    border: 3px solid #818cf8;
-    border-radius: 10px;
-    font-size: 200%;
-    width: 12vw;
-    height: 8vh;
-    background: #818cf8;
-  }
+.CreateButton {
+  border: 3px solid #818cf8;
+  border-radius: 10px;
+  font-size: 200%;
+  width: 12vw;
+  height: 8vh;
+  background: #818cf8;
+}
 
-  #Rules {
-    float: right;
-    border: 1px solid #818cf8;
-    border-radius: 0 0 6px 0;
-    width: 70%;
-    height: 36vh;
-    padding: 10px;
-  }
- 
+#Rules {
+  float: right;
+  border: 1px solid #818cf8;
+  border-radius: 0 0 6px 0;
+  width: 70%;
+  height: 36vh;
+  padding: 10px;
+}
 </style>
-
-
-
