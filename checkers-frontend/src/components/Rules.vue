8eaed0380655bde07dd0411819be872aa7264ad1
Hayk-Tovmasyan <haymaytov@gmail.com>
2024-05-16 08:30:36 +0200

    Rozdzielenie_na_componenty

---
+++ checkers-frontend/src/components/Rules.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import Card from 'primevue/card';
+    
+</script>
+
+<template>
+<Card id="Rules">
+    <template #title>Rules</template>
+    <template #content>
+      <p class="m-0">
+      <ul>
+        <li>Board 8x8</li>
+        <li>Short moves only</li>
+        <li>Cannot capture backwards</li>
+        <li>Capture - choose any</li>
+        <li>Red is playing first</li>
+      </ul>
+      </p>
+    </template>
+
+  </Card>
+
+</template>
