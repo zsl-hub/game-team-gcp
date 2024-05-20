a1a819e07a572a7844539ba395e3c6012bea370b
Maciej Grzesiak <107268659+Mistermakos@users.noreply.github.com>
2024-05-20 12:15:14 +0200

    71 walidacja ruchu uytkownika (#105)

--- checkers-backend/src/repository/otherroutes.repository.ts
+++ checkers-backend/src/repository/otherroutes.repository.ts
@@ -1,5 +1,7 @@
 import { Datastore, PropertyFilter } from '@google-cloud/datastore';
-import { Room } from 'src/dto/room.dto'
+import { Room } from '../dto/room.dto'
+import { PositionMove } from "../dto/gamemoves.dto"
+import { Body } from '@nestjs/common';
 
 export class OtherRoutesRepository {
     datastore = new Datastore({ databaseId: 'checkers-datastore', projectId: "checkers-zsl" });
@@ -9,5 +11,17 @@ export class OtherRoutesRepository {
         const query = this.datastore.createQuery("room").filter(new PropertyFilter("isAvailable", "=", 1));
         const result = await query.run();
         return result[0];
+    }
+
+    async makeMove(@Body() body: PositionMove): Promise<number> {
+        const query = this.datastore.createQuery("gameMove").filter(new PropertyFilter("gameId", "=", body.gameId));
+        const [rooms, queryInfo] = await query.run();
+        const taskKey = rooms[0][this.datastore.KEY];
+        const entity = {
+            key: taskKey,
+            data: body,
+        };
+        await this.datastore.update(entity);
+        return 1;
     }
 }
