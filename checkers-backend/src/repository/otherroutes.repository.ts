030ab59a04e1cf0a863304ef4e81dc215d16d19e
Mistermakos <mistermakos.mcg@gmail.com>
2024-05-20 12:13:18 +0200

    Fixes

--- checkers-backend/src/repository/otherroutes.repository.ts
+++ checkers-backend/src/repository/otherroutes.repository.ts
@@ -1,6 +1,6 @@
 import { Datastore, PropertyFilter } from '@google-cloud/datastore';
 import { Room } from '../dto/room.dto'
-import { GameMove } from "../dto/gamemoves.dto"
+import { PositionMove } from "../dto/gamemoves.dto"
 import { Body } from '@nestjs/common';
 
 export class OtherRoutesRepository {
@@ -13,7 +13,15 @@ export class OtherRoutesRepository {
         return result[0];
     }
 
-    async makeMove(@Body() body: GameMove): Promise<number> {
+    async makeMove(@Body() body: PositionMove): Promise<number> {
+        const query = this.datastore.createQuery("gameMove").filter(new PropertyFilter("gameId", "=", body.gameId));
+        const [rooms, queryInfo] = await query.run();
+        const taskKey = rooms[0][this.datastore.KEY];
+        const entity = {
+            key: taskKey,
+            data: body,
+        };
+        await this.datastore.update(entity);
         return 1;
     }
 }
