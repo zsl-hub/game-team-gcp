91b422d57d28d89d8bda497ce710e1cc8c345fa1
Mikita Leuchyk <100790816+Kotieta@users.noreply.github.com>
2024-05-21 10:50:31 +0200

    107 dodanie cookiesow po wcisnieciu przycisku create join (#120)

--- checkers-frontend/src/router/index.js
+++ checkers-frontend/src/router/index.js
@@ -6,7 +6,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/Game/',
+      path: '/Game/:id',
       name: 'Game',
       component: Game
     },
